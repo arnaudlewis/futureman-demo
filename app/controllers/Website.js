17c4af2e754fd2f382681f0deb79c97bdab4996b
prismic-levi <lga@zengularity.com>
2016-07-26 12:47:35 +0200

    Simplified Prismic Integration
    Added an inline svg, updated the code for prismic writing room
    simplifications, & added missing link check.

--- app/controllers/Website.js
+++ app/controllers/Website.js
@@ -6,7 +6,7 @@ export default {
   index(req, res){
     Prismic.api(req, res)
       .then((api) => {
-        api.getByUID('home', 'home')
+        api.getByUID('home', 'home', {'fetchLinks': 'svg.svg'})
           .then((doc) => res.render('index', {'doc': doc}))
           .catch((err) => res.redirect(Router.notFound))
         })
